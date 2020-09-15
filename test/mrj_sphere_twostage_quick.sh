26bd40cd9f44253be05a3358e060491449ee739e
Timothy Middlemas <tm17@princeton.edu>
2020-09-14 21:28:05 -0400

    Updated some tests

--- test/mrj_sphere_twostage_quick.sh
+++ test/mrj_sphere_twostage_quick.sh
@@ -16,47 +16,48 @@ TEST_DIR=mrj_sphere_twostage_quick
 # up to some threshold in nn_gap, then finish compressing at
 # 1e-1. 1e-2, and 1e-3. Run 5 each.
 
+# https://stackoverflow.com/questions/22768533/d-command-not-found
 (
 RUN_NAME=final_1e-1_1
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 40000 --sweeps 50000 --adjust --gap-threshold 1e-8 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.json --cell-list --pressure 1e100 --pcell 0.0005 --moves 40000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-1_2
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 40000 --sweeps 50000 --adjust --gap-threshold 1e-8 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.json --cell-list --pressure 1e100 --pcell 0.0005 --moves 40000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-1_3
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 40000 --sweeps 50000 --adjust --gap-threshold 1e-8 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.json --cell-list --pressure 1e100 --pcell 0.0005 --moves 40000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-1_4
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 40000 --sweeps 50000 --adjust --gap-threshold 1e-8 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.json --cell-list --pressure 1e100 --pcell 0.0005 --moves 40000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-1_5
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- -d 3 -n 200 -s 16 --cell-list --pressure 1e100 --pcell 0.005 --moves 40000 --sweeps 50000 --adjust --gap-threshold 1e-8 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.json --cell-list --pressure 1e100 --pcell 0.0005 --moves 40000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
--- test/mrj_sphere_twostage_slower.sh
+++ test/mrj_sphere_twostage_slower.sh
@@ -12,47 +12,48 @@ TEST_DIR=mrj_sphere_twostage_slower
 # Also ubuntu default crontab
 [ -d "$TEST_DIR" ] || mkdir "$TEST_DIR"
 
+# https://stackoverflow.com/questions/22768533/d-command-not-found
 (
 RUN_NAME=final_1e-4_1
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 1e-6 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --no-rayon --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.yaml --cell-list --pressure inf --pcell 0.0000005 --moves 40000000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-4_2
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 1e-6 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --no-rayon --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.yaml --cell-list --pressure inf --pcell 0.0000005 --moves 40000000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-4_3
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 1e-6 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --no-rayon --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.yaml --cell-list --pressure inf --pcell 0.0000005 --moves 40000000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-4_4
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 1e-6 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --no-rayon --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.yaml --cell-list --pressure inf --pcell 0.0000005 --moves 40000000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
 
 (
 RUN_NAME=final_1e-4_5
-[-d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
+[ -d "$TEST_DIR/$RUN_NAME" ] || mkdir $TEST_DIR/$RUN_NAME
 
-cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 0.003 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
+cargo run --release -- --no-rayon -d 3 -n 200 -s 16 --cell-list --pressure inf --pcell 0.005 --moves 4000 --sweeps 50000 --adjust --gap-threshold 1e-6 --logfiles $TEST_DIR/$RUN_NAME/initial_log -o $TEST_DIR/$RUN_NAME/initial_config 2>$TEST_DIR/$RUN_NAME/initial.stderr >$TEST_DIR/$RUN_NAME/initial.stdout
 
 cargo run --release -- --no-rayon --initfile $TEST_DIR/$RUN_NAME/initial_config_final.dat --schedulefile $TEST_DIR/$RUN_NAME/initial_log_schedule.yaml --cell-list --pressure inf --pcell 0.0000005 --moves 40000000 --sweeps 5000 --adjust --logfiles $TEST_DIR/$RUN_NAME/final_log -o $TEST_DIR/$RUN_NAME/final_config 2>$TEST_DIR/$RUN_NAME/final.stderr >$TEST_DIR/$RUN_NAME/final.stdout
 ) &
